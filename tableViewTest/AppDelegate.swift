3484d5e61fecfb266e824b35494f1cb09ee1b722
Robin Kunde <rkunde@cardinalsolutions.com>
2017-09-15 14:52:42 -0400

    initial commit

---
+++ tableViewTest/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  tableViewTest
+//
+//  Created by Robin Kunde on 9/15/17.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ tableViewTest/MasterViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MasterViewController.swift
+//  tableViewTest
+//
+//  Created by Robin Kunde on 9/15/17.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+    var objects = [Date()]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isTranslucent = false
+//        tableView.contentInsetAdjustmentBehavior = .never
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object.description
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+}
